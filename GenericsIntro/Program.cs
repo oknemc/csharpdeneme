448171357b4309f87ed80b97ad4f0e5bbf1e0dec
oknemc <100702729+oknemc@users.noreply.github.com>
2022-04-21 18:47:08 +0300

    Deneme

--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -2,3 +2,15 @@
 
 MyList<string> isimler = new MyList<string>();
 isimler.Add("Okan");
+
+Console.WriteLine(isimler.Length);
+
+isimler.Add("Oğuz");
+
+Console.WriteLine(isimler.Length);
+
+foreach (var isim in isimler.Items)
+{
+    Console.WriteLine(isim);
+}
+
