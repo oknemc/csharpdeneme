ad0b9e871f3ce03b7a6b12e4386a1a9a5d1fd11c
oknemc <100702729+oknemc@users.noreply.github.com>
2022-04-21 18:41:54 +0300

    Deneme

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,17 @@
+﻿//string[] isimler = new string[] { "Okan", "Emir", "Uğur" };
+//Console.WriteLine(isimler[0]);
+//Console.WriteLine(isimler[1]);
+//Console.WriteLine(isimler[2]);
+
+//isimler = new string[5];
+//isimler[4] = "İlker";
+//Console.WriteLine(isimler[4]);
+//Console.WriteLine(isimler[0]);
+
+List<string> isimler2 = new List<string> {"Okan","Emir","Uğur"};
+Console.WriteLine(isimler2[0]);
+Console.WriteLine(isimler2[1]);
+Console.WriteLine(isimler2[2]);
+isimler2.Add("Sena");
+Console.WriteLine(isimler2[3]);
+Console.WriteLine(isimler2[0]);
